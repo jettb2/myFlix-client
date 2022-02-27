40e5699a1c792e8996149a24aca02118685e5a0f
jettb2 <jettblaschka@gmail.com>
2022-02-26 21:53:45 -0400

    added branch2

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,6 +5,9 @@ import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
+// import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Row';
 
 
 export class MainView extends React.Component {
@@ -53,14 +56,26 @@ export class MainView extends React.Component {
         if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
 
 
-
         return (
             <div className="main-view">
                 {selectedMovie
-                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                    : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie); }} />
-                    ))}
+                    ? (
+                        <Row className="justify-content-md-center">
+                            <Col md={8}>
+                                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                            </Col>
+                        </Row>
+                    )
+                    : (
+                        <Row className="justify-content-md-center">
+                            {movies.map(movie => (
+                                <Col md={3}>
+                                    <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                                </Col>
+                            ))}
+                        </Row>
+                    )
+                }
             </div>
         );
     }
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -20,19 +20,19 @@ export class MovieCard extends React.Component {
     }
 }
 
-MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        Title: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        Genre: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Description: PropTypes.string.isRequired
-        }),
-        Director: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Bio: PropTypes.string.isRequired
-        }),
-        ImagePath: PropTypes.string
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
-};
+// MovieCard.propTypes = {
+//     movie: PropTypes.shape({
+//         Title: PropTypes.string.isRequired,
+//         Description: PropTypes.string.isRequired,
+//         Genre: PropTypes.shape({
+//             Name: PropTypes.string.isRequired,
+//             Description: PropTypes.string.isRequired
+//         }),
+//         Director: PropTypes.shape({
+//             Name: PropTypes.string.isRequired,
+//             Bio: PropTypes.string.isRequired
+//         }),
+//         ImagePath: PropTypes.string
+//     }).isRequired,
+//     onMovieClick: PropTypes.func.isRequired
+// };
